cc3cc0525cf6fe544c499cc9ece296e19d97cc92
Djiar Salim <djiar@kth.se>
2019-12-19 14:39:25 +0100

    add draft of how we will get the data

---
+++ dataProvider.py
@@ -0,0 +1,19 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+
+style.use('ggplot')
+
+#Usage: start time, end time , how long period, stock 1 , stock 2 , stock 3 and so on
+#output is [[period 1], [period 2], ...]
+def get_data(start_time,end_time,period_length,*stock_names):
+    pass
+
+
+#Usage: give me one example training data and I will label it
+#output: Label , Buy/Sell.
+def simple_classifier():
+    pass
